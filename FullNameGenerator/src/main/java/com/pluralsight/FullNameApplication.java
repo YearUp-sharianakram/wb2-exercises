1fb68a4d659fb9d130dddf7b1034677ee2a1f429
sharianakram <sharianakram0503@gmail.com>
2024-10-02 12:11:27 -0400

    Completed Exercise FullNameApplication

---
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        System.out.println("Please enter your name");
+        String firstName = (promptForString("First name: ")).trim();
+        String middleName = (promptForString("Middle name: ")).trim();
+        String lastName = (promptForString("Last name: ")).trim();
+        String suffix = (promptForString("Suffix: ")).trim();
+        String fullName = firstName;
+        if (!middleName.isEmpty()){
+            fullName = fullName + " " + middleName;
+        }
+        fullName = fullName + " " + lastName;
+        if (!suffix.isEmpty()){
+            fullName = fullName + ", " + suffix;
+        }
+        System.out.println("Full Name: " + fullName);
+    }
+
+    public static String promptForString (String prompt){
+        System.out.println(prompt);
+        return myScanner.nextLine();
+    }
+
+//    public static
+
+}
