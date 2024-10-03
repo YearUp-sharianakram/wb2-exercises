4e086901a8129756c54d143e0fcd31ce782b3ed8
sharianakram <sharianakram0503@gmail.com>
2024-10-03 13:34:13 -0400

    Completed Theater Reservations

--- TheaterReservations/src/main/java/com/pluralsight/Main.java
+++ TheaterReservations/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,33 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fullName = promptForString("Please enter your first and last name: ");
+        String dateUserEntry = promptForString("What date will you be coming (MM/dd/yyyy)? ");
+        int ticketAmount = promptForInt("How many tickets would you like? ");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate date = LocalDate.parse(dateUserEntry, formatter);
+        String ticketWord = (ticketAmount == 1) ? "ticket" : "tickets";
+        String[] names = fullName.split(" ");
+        System.out.println(ticketAmount + " "  + ticketWord + " reserved for " + date + " under " + names[1] + ", " + names[0] );
     }
+
+    public static String promptForString (String prompt){
+        System.out.println(prompt);
+        return myScanner.nextLine().trim();
+    }
+    public static int promptForInt(String prompt){
+        System.out.println(prompt);
+        int num = myScanner.nextInt();
+        myScanner.nextLine();
+        return num;
+    }
+
+
 }
