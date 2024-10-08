8c97eec91d77803834f8d3fb3bbec3f2c489ad60
sharianakram <sharianakram0503@gmail.com>
2024-10-08 19:23:06 -0400

    completed Exercise with Bonus

--- TestStatistics/src/main/java/com/pluralsight/Main.java
+++ TestStatistics/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,56 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] testScores = {1,1,1,1,1,1,1,1,1,100000};
+        Arrays.sort(testScores);
+        System.out.println("The average is " + average(testScores));
+        System.out.println("The highest score is " + highScore(testScores));
+        System.out.println("The low score is " + lowScore(testScores));
+        System.out.println("The median is " + median(testScores));
+        System.out.println("The difference between average and median is " + (average(testScores) - median(testScores)));
+
+
+
+    }
+
+    public static double average(int[] testScores){
+        int total = 0;
+        for (int score : testScores){
+            total += score;
+        }
+        return (double) total /testScores.length;
+    }
+
+    public static int highScore(int[] testScores){
+        int currentHighest = 0;
+        for (int score : testScores){
+            if (score > currentHighest){
+                currentHighest = score;
+            }
+        }
+        return currentHighest;
+    }
+
+    public static int lowScore(int[] testScores){
+        int currentLowest = 1000000;
+        for (int score : testScores){
+            if (score < currentLowest){
+                currentLowest = score;
+            }
+        }
+        return currentLowest;
+    }
+
+    public static double median(int[] testScores){
+        int middleNumber = (int)(testScores.length / 2);
+        if (testScores.length % 2 == 1){
+            return testScores[middleNumber];
+        }else{
+            return ((double) (testScores[middleNumber - 1] + testScores[middleNumber]) / 2);
+
+        }
     }
 }
